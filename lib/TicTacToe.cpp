c61d110fda8bb9e81c45f1bd10fdb4248c1f45b9
Клаус Владимир <klausvovan007@gmail.com>
2022-10-19 23:01:31 +0200

    upgrading tic-tac-toe interface

--- lib/TicTacToe.cpp
+++ lib/TicTacToe.cpp
@@ -8,9 +8,10 @@
 #include "TicTacToe.hpp"
 #include "Constants.hpp"
 
-void draw(SDL_Rect tQuad, int indX, int indY, SDL_Texture* dTexture,
+void draw(SDL_Rect tQuad, int ind, SDL_Texture* dTexture,
 	SDL_Renderer* gRenderer)
 {
+	int indX = ind%3, indY = ind/3;
 	tQuad.x += (tQuad.w+55)*indX;
 	tQuad.y += (tQuad.h+55)*indY;
 	SDL_RenderCopy( gRenderer, dTexture, NULL, &tQuad );
@@ -20,6 +21,9 @@ int ticTacToe(SDL_Renderer* gRenderer, SDL_Texture* tTexture,
 	SDL_Texture* fieldTexture, SDL_Texture* x1Texture,
 	SDL_Texture* xTexture, SDL_Texture* oTexture)
 {
+	vector <int> val = {0,0,0,
+						0,0,0,
+						0,0,0};
 	SDL_Rect tQuad;
 
 	tQuad.x = 100;
@@ -38,6 +42,7 @@ int ticTacToe(SDL_Renderer* gRenderer, SDL_Texture* tTexture,
 	pair<int,int> p8;
 	pair<int,int> p9;
 	int ans = 0;
+	int ind = 0;
 	bool quit = false;
 	while( !quit )
 	{
@@ -52,15 +57,51 @@ int ticTacToe(SDL_Renderer* gRenderer, SDL_Texture* tTexture,
 			else if(e.type == SDL_KEYDOWN && e.key.repeat == 0){
 				switch( e.key.keysym.sym )
 				{
+					case SDLK_d:
+					case SDLK_RIGHT:
+						if(ind == 8){
+							ind = 0;
+						}
+						else{
+							ind += 1;
+						}
+					break;
+
+					case SDLK_a:
+					case SDLK_LEFT:
+						if(ind == 0){
+							ind = 8;
+						}
+						else{
+							ind -= 1;
+						}
+					break;
+
 					case SDLK_w:
 					case SDLK_UP:
+						if(ind/3 == 0){
+							ind+=6;
+						}
+						else{
+							ind-=3;
+						}
 					break;
 
 					case SDLK_s:
 					case SDLK_DOWN:
+						if(ind/3 == 2){
+							ind-=6;
+						}
+						else{
+							ind+=3;
+						}
 					break;
 
 					case SDLK_SPACE:
+					case SDLK_RETURN:
+					case SDLK_RETURN2:
+						if(!val[ind])
+							val[ind] = 1;
 					break;
 
 					case SDLK_ESCAPE:
@@ -70,13 +111,25 @@ int ticTacToe(SDL_Renderer* gRenderer, SDL_Texture* tTexture,
 				}
 			}
 		}
+
+	int indX = ind%3, indY = ind/3;
+	tQuad.x = 100 + (tQuad.w+55)*indX;
+	tQuad.y = 100 + (tQuad.h+55)*indY;
+
 	SDL_RenderCopy( gRenderer, tTexture, NULL, NULL );
 	SDL_RenderCopy( gRenderer, fieldTexture, NULL, NULL );
+
+	for(int i = 0;i<val.size();++i){
+		if(val[i] == -1){
+			draw({100,100,tQuad.w,tQuad.h},i,oTexture,gRenderer);
+		}
+		else if(val[i] == 1){
+			draw({100,100,tQuad.w,tQuad.h},i,xTexture,gRenderer);
+		}
+	}
+
 	SDL_RenderCopy( gRenderer, x1Texture, NULL, &tQuad );
-	draw({tQuad.x,tQuad.y,tQuad.w,tQuad.h},1,0,oTexture,gRenderer);
-	draw({tQuad.x,tQuad.y,tQuad.w,tQuad.h},2,0,xTexture,gRenderer);
-	draw({tQuad.x,tQuad.y,tQuad.w,tQuad.h},1,1,xTexture,gRenderer);
-	draw({tQuad.x,tQuad.y,tQuad.w,tQuad.h},0,2,xTexture,gRenderer);
+
 	SDL_RenderPresent( gRenderer );
 	SDL_Delay(5);
 	}
